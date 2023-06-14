569597ef1028010a03879bb3829a917153cc6cb6
Nayeem Hyder Riddhi <riddhi00121@gmail.com>
2023-06-14 11:49:28 +0600

    commit

---
+++ awsl1/index.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk');
+
+const iam = new AWS.IAM();
+
+const params = {
+    UserName: 'nayeem'
+};
+
+iam.createUser(params, function(err, data){
+
+    if(err){
+        console.log(err);
+    }
+    else {
+        console.log(data);
+    }
+
+})
